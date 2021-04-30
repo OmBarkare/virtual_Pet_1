0e877cfb3a27c978e96aacca92fb5f191c1bc67e
OmBarkare <om062006@gmail.com>
2021-04-30 16:04:09 +0530

    adding Images

--- sketch.js
+++ sketch.js
@@ -3,35 +3,44 @@ var foodStock;
 var food, foodStockRef, foodClass;
 var test, testRef;
 var foodCount;
+var dogMood;
 
-function preload()
-{
-	//load images here
+function preload(){
+  dogImg = loadImage("images/dogImg.png");
+  happyDogImg = loadImage("images/dogImg1.png");
 }
 
 function setup() {
 	createCanvas(800, 700);
   foodObject = new Food();
+  dogMood = "hungry";
 // testFunction();
 }
 
 
 function draw() {  
   background(0);
+
   foodObject.getFoodStock();
   text("food: "+foodStock, 200, 200);
-//text("test"+test,100,100);
-//Form.display();
-  text("press space to feed the dog",400,100);
-}
 
-/*async function testFunction(){
-  testRef = database.ref("Test");
-  await testRef.on("value",function(data){test = data.val();});
-}*/
+  dog = createSprite(500,350,50,50);
+
+  if(dogMood === "hungry"){
+    dog.addImage(dogImg);
+  }
+
+  dog.scale = 0.3;
+
+  text("press space to feed the dog",400,100);
+
+  drawSprites();
+}
 
 function keyPressed(){
   if(keyCode===32){
     foodObject.update(foodStock);
+    dogMood = "happy";
+    dog.addImage("happy",happyDogImg);
   }  
 }
